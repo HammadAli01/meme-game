0bbb3c40375e4b58b050861a9308b6ba619f809d
Zunnooran <zunnooran.alvi@gmail.com>
2024-06-12 16:51:18 +0500

    feat-changes

--- src/routes/private-routes.tsx
+++ src/routes/private-routes.tsx
@@ -4,6 +4,7 @@ import { Character } from 'pages/character/Character';
 import { FightPlay } from 'pages/fight-play/FightPlay';
 import Home from 'pages/home/home';
 import { MainMenu } from 'pages/mainMenu/MainMenu';
+import Refer from 'pages/reward/refer';
 import { StartingFight } from 'pages/starting-fight/StartingFight';
 
 function PrivateRoutes() {
@@ -13,6 +14,7 @@ function PrivateRoutes() {
       <Route path='character' element={<Character />} />
       <Route path='starting-fight' element={<StartingFight />} />
       <Route path='fight' element={<FightPlay />} />
+      <Route path='reward' element={<Refer />} />
       <Route path='/home' element={<Home />} />
 
       <Route path='*' element={<Navigate to='/error/404' />} />
